85f01de4191a484c9ce6ed2a46500f57743f2077
Dawn Kline <dawn.kline.m@gmail.com>
2023-05-09 23:43:35 -0500

    updated models folder, api folder

--- Develop/models/Tag.js
+++ Develop/models/Tag.js
@@ -6,7 +6,16 @@ class Tag extends Model { }
 
 Tag.init(
   {
-    // define columns
+    // Define columns for the tag table
+    id: {
+      type: DataTypes.INTEGER, // Set the data type to INTEGER
+      allowNull: false, // Disallow NULL values
+      primaryKey: true, // Set as the primary key
+      autoIncrement: true, // Automatically increment the value for each new record
+    },
+    tag_name: {
+      type: DataTypes.STRING, // Set the data type to STRING
+    },
   },
   {
     sequelize,
